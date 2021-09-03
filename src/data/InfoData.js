178548e91e921bfbba22db8cc095091f8cde9b53
cubbije@gmail.com <cubbije@github,com>
2021-09-03 16:00:51 +0300

    first-com

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/5-int.jpg";
+import ImageTwo from "../images/5.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. ",
+  paragraphTwo:
+    "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  paragraphTwo:
+    "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. ",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from "../images/1.jpg";
+import ImageTwo from "../images/2.jpg";
+import ImageThree from "../images/3.jpg";
+import ImageFour from "../images/4.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    patch: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,860,000",
+    patch: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8,420,000",
+    patch: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Hounston, Texas",
+    price: "$6,660,000",
+    patch: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: "House in Las Vegas, Nevada",
+//     price: "$8,980,000",
+//     patch: "/homes",
+//     label: "View Home",
+//     image: ImageFive,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Houston, Texas",
+//     price: "$3,460,000",
+//     patch: "/homes",
+//     label: "View Home",
+//     image: ImageSix,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Alajuela, Costa Rica",
+//     price: "$5,420,000",
+//     patch: "/homes",
+//     label: "View Home",
+//     image: ImageSeven,
+//     alt: "House",
+//   },
+// ];
